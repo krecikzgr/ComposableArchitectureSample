0ad9218f44f3219f2fb5ee66d0ca86baf995a3fd
adrian.kaleta <adrian.kaleta>
2024-01-13 21:43:55 +0100

    Init

---
+++ TheComposableArchitectureSample/TheComposableArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheComposableArchitectureSampleApp.swift
+//  TheComposableArchitectureSample
+//
+//  Created by Adrian Kaleta on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheComposableArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
